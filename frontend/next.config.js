9b54319766afd8c9fceb8291eb7f544fc3dfe214
Nawaf <smn11789@gmail.com>
2025-03-23 05:23:21 +0300

    F

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    i18n: {
-      // دعم اللغة العربية كلغة افتراضية
-      locales: ['ar'],
-      defaultLocale: 'ar',
-    }
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  i18n: {
+    locales: ['ar'],
+    defaultLocale: 'ar',
+  },
+  eslint: {
+    // تخطي فحص ESLint أثناء بناء المشروع
+    ignoreDuringBuilds: true,
+  },
+}
+
+module.exports = nextConfig
